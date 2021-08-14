998163b44632d96654bdb23c31d77aae119b9237
Xavier Paredes <jxavierpp1@gmail.com>
2021-08-14 06:28:10 +0000

    🎨 Fix format

--- app/src/components/MessageListItem.vue
+++ app/src/components/MessageListItem.vue
@@ -1,29 +1,44 @@
 <template>
-  <ion-item v-if="message" :routerLink="'/message/' + message.id" :detail="false" class="list-item">
+  <ion-item
+    v-if="message"
+    :router-link="'/message/' + message.id"
+    :detail="false"
+    class="list-item"
+  >
     <div slot="start" :class="!message.read ? 'dot dot-unread' : 'dot'"></div>
     <ion-label class="ion-text-wrap">
       <h2>
         {{ message.fromName }}
         <span class="date">
           <ion-note>{{ message.date }}</ion-note>
-          <ion-icon :icon="chevronForward" size="small" v-if="isIos()"></ion-icon>
+          <ion-icon
+            v-if="isIos()"
+            :icon="chevronForward"
+            size="small"
+          ></ion-icon>
         </span>
       </h2>
       <h3>{{ message.subject }}</h3>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
       </p>
     </ion-label>
   </ion-item>
 </template>
 
 <script lang="ts">
-import { IonIcon, IonItem, IonLabel, IonNote } from '@ionic/vue';
-import { chevronForward } from 'ionicons/icons';
-import { defineComponent } from 'vue';
+import { IonIcon, IonItem, IonLabel, IonNote } from "@ionic/vue";
+import { chevronForward } from "ionicons/icons";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'MessageListItem',
+  name: "MessageListItem",
   components: {
     IonIcon,
     IonItem,
@@ -33,15 +48,15 @@ export default defineComponent({
   props: {
     message: Object,
   },
+  data() {
+    return { chevronForward };
+  },
   methods: {
     isIos: () => {
       const win = window as any;
-      return win && win.Ionic && win.Ionic.mode === 'ios';
-    }
+      return win && win.Ionic && win.Ionic.mode === "ios";
+    },
   },
-  data() {
-    return { chevronForward }
-  }
 });
 </script>
 
@@ -56,7 +71,7 @@ export default defineComponent({
   margin-bottom: 12px;
 }
 
-.list-item  h2 {
+.list-item h2 {
   font-weight: 600;
   margin: 0;
 }
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -5,45 +5,46 @@
         <ion-title>Inbox</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-refresher slot="fixed" @ionRefresh="refresh($event)">
         <ion-refresher-content></ion-refresher-content>
       </ion-refresher>
-      
+
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Inbox</ion-title>
         </ion-toolbar>
       </ion-header>
-      
+
       <ion-list>
-        <MessageListItem v-for="message in messages" :key="message.id" :message="message" />
+        <MessageListItem
+          v-for="message in messages"
+          :key="message.id"
+          :message="message"
+        />
       </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonList, IonPage, IonRefresher, IonRefresherContent, IonTitle, IonToolbar } from '@ionic/vue';
-import MessageListItem from '@/components/MessageListItem.vue';
-import { defineComponent } from 'vue';
-import { getMessages } from '@/data/messages';
+import {
+  IonContent,
+  IonHeader,
+  IonList,
+  IonPage,
+  IonRefresher,
+  IonRefresherContent,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import MessageListItem from "@/components/MessageListItem.vue";
+import { defineComponent } from "vue";
+import { getMessages } from "@/data/messages";
 
 export default defineComponent({
-  name: 'Home',
-  data() {
-    return {
-      messages: getMessages()
-    }
-  },
-  methods: {
-    refresh: (ev: CustomEvent) => {
-      setTimeout(() => {
-        ev.detail.complete();
-      }, 3000);
-    }
-  },
+  name: "Home",
   components: {
     IonContent,
     IonHeader,
@@ -53,7 +54,19 @@ export default defineComponent({
     IonRefresherContent,
     IonTitle,
     IonToolbar,
-    MessageListItem
+    MessageListItem,
+  },
+  data() {
+    return {
+      messages: getMessages(),
+    };
+  },
+  methods: {
+    refresh: (ev: CustomEvent) => {
+      setTimeout(() => {
+        ev.detail.complete();
+      }, 3000);
+    },
   },
 });
 </script>
