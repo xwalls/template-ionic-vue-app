a425c01bdae6a37a0b37a52fee05a9914426d0d6
Xavier Paredes <jxavierpp1@gmail.com>
2021-08-19 06:57:18 +0000

    ✨ Use service worker and vue store

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "./registerServiceWorker";
 import router from './router'
+import store from "./store";
 
 import { IonicVue } from '@ionic/vue'
 
@@ -23,7 +25,7 @@ import '@ionic/vue/css/display.css'
 /* Theme variables */
 import './theme/variables.css'
 
-const app = createApp(App).use(IonicVue).use(router)
+const app = createApp(App).use(IonicVue).use(store).use(router)
 
 router.isReady().then(() => {
   app.mount('#app')
